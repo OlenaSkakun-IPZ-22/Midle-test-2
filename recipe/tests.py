da48ee4f66810a557f9ed2ec1d8776fc38d6ca04
OlenaSkakun-IPZ-22 <olena_skakunn@knu.ua>
2025-05-21 12:48:12 +0300

    створено тести та виправлено помилки

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Category, Recipe
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_category_creation_and_iter(self):
+        cat = Category.objects.create(name="Салати")
+        self.assertEqual(str(cat), "Салати")
+        self.assertIn("Салати", list(iter(cat)))
+
+
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        self.cat = Category.objects.create(name="Основні страви")
+
+    def test_recipe_creation(self):
+        recipe = Recipe.objects.create(
+            title="Борщ",
+            description="Традиційна страва",
+            instructions="Готувати 2 години",
+            ingredients="буряк, капуста, картопля",
+            category=self.cat
+        )
+        self.assertEqual(str(recipe), "Борщ")
+        self.assertEqual(recipe.category.name, "Основні страви")
+
